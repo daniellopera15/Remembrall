1b0a795e534d0ec3b2f15b8f44fbbc66e01a082e
Daniel Lopera <daniel.lopera@despegar.com>
2022-05-10 01:39:16 -0300

    Init repo

---
+++ src/main/java/com/optica/loi/remembrall/RemembrallApplication.java
@@ -0,0 +1,13 @@
+package com.optica.loi.remembrall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemembrallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemembrallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optica/loi/remembrall/RemembrallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optica.loi.remembrall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemembrallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
